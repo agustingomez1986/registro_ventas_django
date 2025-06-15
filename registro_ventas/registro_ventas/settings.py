4eedbc041afc08373a8f553987e3c61eccc914b0
Agustin <agustingomez1986@hotmail.com>
2025-06-15 22:20:10 +0200

    Estilo css a base.html

--- registro_ventas/registro_ventas/settings.py
+++ registro_ventas/registro_ventas/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static", # para que cargue desde la carpeta static global
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
